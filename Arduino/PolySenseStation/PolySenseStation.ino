4e1d71142f93a80e13de26d29dc878561e7566da
Terrasfear <caspervanderhorst@gmail.com>
2018-01-26 17:14:20 +0100

    Add files via upload

---
+++ Arduino/PolySenseStation/PolySenseStation.ino
@@ -0,0 +1,135 @@
+#include <SoftwareSerial.h>
+#include <CmdMessenger.h>
+#include <ESP8266WiFi.h>
+#include <PubSubClient.h>
+
+const char* ssid = "Minor_IE";
+const char* password = "Minor2017";
+IPAddress server(192, 168, 2, 111);
+WiFiClient espClient;
+PubSubClient client(espClient);
+SoftwareSerial connection(D3, D4);
+CmdMessenger cmdMessenger = CmdMessenger(connection);
+
+const int VOID = 0;
+const int NOECHO = 1;
+const int LUMO = 2;
+const int POLYSENSE = 3;
+const int IRIS = 4;
+
+const long timeout = 500;
+long timeStamp = 0;
+long lastCheck = 0;
+int oldRotation = 0;
+boolean connexted = false;
+int connextId;
+char msg[50];
+
+enum {
+  kStationId,
+  kConnextId,
+  kConnected
+};
+
+void attachCommandCallbacks() {
+  cmdMessenger.attach(kConnextId, onConnextId);
+  cmdMessenger.attach(kConnected, onConnected);
+}
+
+void onConnextId() {
+  connextId = cmdMessenger.readInt16Arg();
+  cmdMessenger.sendCmd(kConnected);
+  Serial.println("Connext " + String(connextId) + " connected");
+  itoa(connextId, msg, 10);
+  client.publish("polysense", msg);
+}
+
+void onConnected() {
+  connexted = true;
+  timeStamp = millis();
+}
+
+void setup() {
+  Serial.begin(4800);
+  connection.begin(9600);
+  attachCommandCallbacks();
+  setup_wifi();
+  client.setServer(server, 1883);
+  client.setCallback(callback);
+}
+
+void loop() {
+  cmdMessenger.feedinSerialData();
+  handleConnection();
+  handleWifi();
+}
+
+void handleConnection() {
+  if (!connexted) {
+    cmdMessenger.sendCmd(kStationId, POLYSENSE);
+  } else if (millis() - timeout > timeStamp) {
+    connexted = false;
+    itoa(0, msg, 10);
+    client.publish("polysense", msg);
+    Serial.println("Disconnected");
+  } else {
+    cmdMessenger.sendCmd(kConnected);
+  }
+  if (millis() - lastCheck > 1000 && connexted == false){
+    lastCheck = millis();
+    itoa(0, msg, 10);
+    client.publish("polysense", msg);
+  }
+  delay(50);
+}
+
+void handleWifi() {
+  if (!client.connected()) {
+    reconnect();
+  }
+  client.loop();
+}
+
+void setup_wifi() {
+
+  delay(10);
+  Serial.println();
+  Serial.print("Connecting to ");
+  WiFi.begin(ssid, password);
+
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+
+  Serial.println("");
+  Serial.println("WiFi connected");
+  Serial.println("IP address: ");
+  Serial.println(WiFi.localIP());
+}
+
+void callback(char* topic, byte* payload, unsigned int length) {
+  Serial.print("Message arrived [");
+  Serial.print(topic);
+  Serial.print("] ");
+  for (int i = 0; i < length; i++) {
+    Serial.print((char)payload[i]);
+  }
+  Serial.println();
+}
+
+void reconnect() {
+  while (!client.connected()) {
+    Serial.print("Attempting MQTT connection...");
+    if (client.connect("PolySenseStationClient")) {
+      Serial.println("connected");
+      client.publish("polysenseDebug", "Connext station online");
+    } else {
+      Serial.print("failed, rc=");
+      Serial.print(client.state());
+      Serial.println(" try again in 5 seconds");
+      delay(5000);
+    }
+  }
+}
+
---
+++ Arduino/WemosMQTT/WemosMQTT.ino
@@ -0,0 +1,216 @@
+/*
+  Basic ESP8266 MQTT example
+
+  This sketch demonstrates the capabilities of the pubsub library in combination
+  with the ESP8266 board/library.
+
+  It connects to an MQTT server then:
+  - publishes "hello world" to the topic "outTopic" every two seconds
+  - subscribes to the topic "inTopic", printing out any messages
+    it receives. NB - it assumes the received payloads are strings not binary
+  - If the first character of the topic "inTopic" is an 1, switch ON the ESP Led,
+    else switch it off
+
+  It will reconnect to the server if the connection is lost using a blocking
+  reconnect function. See the 'mqtt_reconnect_nonblocking' example for how to
+  achieve the same result without blocking the main loop.
+
+  To install the ESP8266 board, (using Arduino 1.6.4+):
+  - Add the following 3rd party board manager under "File -> Preferences -> Additional Boards Manager URLs":
+       http://arduino.esp8266.com/stable/package_esp8266com_index.json
+  - Open the "Tools -> Board -> Board Manager" and click install for the ESP8266"
+  - Select your ESP8266 in "Tools -> Board"
+
+*/
+
+#include <ESP8266WiFi.h>
+
+#include <DNSServer.h>
+#include <ESP8266WebServer.h>
+#include <WiFiManager.h>
+
+#include <PubSubClient.h>
+
+// Update these with values suitable for your network.
+
+//const char* ssid = "Minor_IE";
+//const char* password = "Minor2017";
+IPAddress server(192, 168, 2, 111);
+
+WiFiClient espClient;
+PubSubClient client(espClient);
+long lastChange = 0;
+const long personalTimeout = 100;
+char debugMsg[50];
+int value = 0;
+
+const byte dataPins[4] = {D1, D2, D3, D4}; // connect to {2, 3, 4, 5} on acturator Arduino
+bool dataState[4];
+
+void setup() {
+  pinMode(BUILTIN_LED, OUTPUT);     // Initialize the BUILTIN_LED pin as an output
+
+  for (int i = 0; i < 4; i++) {
+    pinMode(dataPins[i], OUTPUT);
+    digitalWrite(dataPins[i], HIGH);
+  }
+
+  Serial.begin(115200);
+
+  WiFiManager wifiManager;
+  wifiManager.autoConnect("Polysense", "tunnel");
+
+
+  //setup_wifi();
+  client.setServer(server, 1883);
+  client.setCallback(callback);
+
+}
+/*
+  void setup_wifi() {
+
+  delay(10);
+  // We start by connecting to a WiFi network
+  Serial.println();
+  Serial.print("Connecting to ");
+  //Serial.println(ssid);
+
+  WiFi.begin(ssid, password);
+
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+
+  Serial.println("");
+  Serial.println("WiFi connected");
+  Serial.println("IP address: ");
+  Serial.println(WiFi.localIP());
+  }
+*/
+void callback(char* topic, byte* payload, unsigned int length) {
+
+  String inString = "";
+
+  Serial.print("Message arrived [ID: ");
+  for (int i = 0; i < length; i++) {
+    Serial.print((char)payload[i]);
+
+    if (isDigit(payload[i])) {
+      inString += (char)payload[i];
+    }
+  }
+
+  Serial.println("]");
+
+  snprintf(debugMsg, 75, "Recieved ID:%ld", inString.toInt());
+  client.publish("polysenseDebug", debugMsg);
+
+  if (dataState[0] == false) {
+    dataState[0] = true;
+    lastChange = millis();
+
+    switch (inString.toInt()) {
+      case 6:
+        dataState[1] = false;
+        dataState[2] = false;
+        dataState[3] = false;
+        break;
+      case 8:
+        dataState[1] = true;
+        dataState[2] = false;
+        dataState[3] = false;
+        break;
+      case 14:
+        dataState[1] = false;
+        dataState[2] = true;
+        dataState[3] = false;
+        break;
+      case 22:
+        dataState[1] = true;
+        dataState[2] = true;
+        dataState[3] = false;
+        break;
+      case 49:
+        dataState[1] = false;
+        dataState[2] = false;
+        dataState[3] = true;
+        break;
+      case 66:
+        dataState[1] = true;
+        dataState[2] = false;
+        dataState[3] = true;
+        break;
+      case 75:
+        dataState[1] = false;
+        dataState[2] = true;
+        dataState[3] = true;
+        break;
+      case 90:
+        dataState[1] = true;
+        dataState[2] = true;
+        dataState[3] = true;
+        break;
+      default:
+        dataState[0] = false;
+        client.publish("polysenseDebug", "unknown ID");
+        break;
+    }
+
+  } else if (inString.toInt() == 0) {
+    dataState[0] = false;
+    client.publish("polysenseDebug", "disconnext");
+  }
+
+}
+
+void reconnect() {
+  // Loop until we're reconnected
+  while (!client.connected()) {
+    Serial.print("Attempting MQTT connection...");
+    // Attempt to connect
+    if (client.connect("PolysensClient")) {
+      Serial.println("connected");
+      // Once connected, publish an announcement...
+      client.publish("polysenseDebug", "Online");
+      // ... and resubscribe
+      client.subscribe("polysense");
+    } else {
+      Serial.print("failed, rc=");
+      Serial.print(client.state());
+      Serial.println(" try again in 5 seconds");
+      // Wait 5 seconds before retrying
+      delay(5000);
+    }
+  }
+}
+void loop() {
+
+  if (!client.connected()) {
+    reconnect();
+  }
+  /*
+    if (millis() - lastChange > personalTimeout) {
+      dataState[0] = false;
+    }*/
+
+  client.loop();
+
+  for (int i = 0; i < 4; i++) {
+    digitalWrite(dataPins[i], dataState[i]);
+  }
+
+
+  /*
+    long now = millis();
+    if (now - lastMsg > 2000) {
+      lastMsg = now;
+      ++value;
+      snprintf (msg, 75, "hello world #%ld", value);
+      Serial.print("Publish message: ");
+      Serial.println(msg);
+      client.publish("outTopic", msg);
+    }
+  */
+}
+
